8e4ba03982a9f339fc71b9d8036d48e07bd98918
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2018-08-22 11:39:48 -0500

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles
+  resources :articles do
+    resources :comments #this is a nested resource
+  end
 
   root 'welcome#index'
 
